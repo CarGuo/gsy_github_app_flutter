dfecf113dda3d27fe7c9915716697d135794d2d2
guoshuyu <359369982@qq.com>
2018-07-09 17:13:09 +0800

    redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:gsy_github_app_flutter/common/style/GSYStyle.dart';
 import 'package:gsy_github_app_flutter/page/WelcomePage.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
-void main() => runApp(new MyApp());
+// One simple action: Increment
+enum Actions { Increment }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// The reducer, which takes the previous count and increments it in response
+// to an Increment action.
+int counterReducer(int state, dynamic action) {
+  if (action == Actions.Increment) {
+    return state + 1;
+  }
+
+  return state;
+}
+
+
+void main() {
+  final store = new Store<int>(counterReducer, initialState: 0);
+
+  runApp(new FlutterReduxApp(
+    title: 'Flutter Redux Demo',
+    store: store,
+  ));
+}
+
+class FlutterReduxApp extends StatelessWidget {
+  final Store<int> store;
+  final String title;
+
+  FlutterReduxApp({Key key, this.store, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: GSYColors.primarySwatch,
-      ),
-      home: new WelcomePage());
+    return new StoreProvider<int>(
+      store: store,
+      child:new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: GSYColors.primarySwatch,
+        ),
+        home: new WelcomePage()),
+    );
   }
 }
