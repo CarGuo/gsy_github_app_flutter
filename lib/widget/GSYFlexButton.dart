93b44f3ae03b4ef15d25aae7741ca1ad4e74c357
guoshuyu <359369982@qq.com>
2018-07-27 09:53:28 +0800

    flex button

--- lib/widget/GSYFlexButton.dart
+++ lib/widget/GSYFlexButton.dart
@@ -18,18 +18,19 @@ class GSYFlexButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Row(children: <Widget>[
-      new Expanded(
-          child: new RaisedButton(
-              padding: new EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0),
-              textColor: textColor,
-              color: color,
-              child: new Text(text, style: new TextStyle(fontSize: 20.0)),
-              onPressed: () {
-                if (this.onPress != null) {
-                  this.onPress();
-                }
-              }))
-    ]);
+    return new RaisedButton(
+        padding: new EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0),
+        textColor: textColor,
+        color: color,
+        child: new Flex(
+          mainAxisAlignment: MainAxisAlignment.center,
+          direction: Axis.horizontal,
+          children: <Widget>[new Text(text, style: new TextStyle(fontSize: 20.0))],
+        ),
+        onPressed: () {
+          if (this.onPress != null) {
+            this.onPress();
+          }
+        });
   }
 }
