aa54aa2e16653d902d310f65880aa221a3c64a36
guoshuyu <359369982@qq.com>
2018-07-30 15:04:53 +0800

    调整头像

---
+++ lib/widget/GSYUserIconWidget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/**
+ * 头像Icon
+ * Created by guoshuyu
+ * Date: 2018-07-30
+ */
+
+class GSYUserIconWidget extends StatelessWidget {
+  final String image;
+  final VoidCallback onPressed;
+  final double width;
+  final double height;
+  final EdgeInsetsGeometry padding;
+
+  GSYUserIconWidget({this.image, this.onPressed, this.width = 30.0, this.height = 30.0, this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    return new RawMaterialButton(
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        padding: padding ?? const EdgeInsets.only(top: 4.0, right: 5.0, left: 5.0),
+        constraints: const BoxConstraints(minWidth: 0.0, minHeight: 0.0),
+        child: new ClipOval(
+          child: new FadeInImage.assetNetwork(
+            placeholder: "static/images/logo.png",
+            //预览图
+            fit: BoxFit.fitWidth,
+            image: image,
+            width: width,
+            height: height,
+          ),
+        ),
+        onPressed: onPressed);
+  }
+}
