bf5faf0985e7b24b9bb1b6f3d34392ddf739bb37
guoshuyu <359369982@qq.com>
2018-08-02 15:01:55 +0800

    增加测试代码

---
+++ lib/test/DemoWidget.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class DEMOWidget extends StatelessWidget {
+  final String text;
+
+  DEMOWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    //这里返回你需要的控件
+    //这里末尾有没有的逗号，对于格式化代码而已是不一样的。
+    return Container(
+      child: Text(text ?? "这就是无状态DMEO"),
+    );
+  }
+}
+
+class DemoStateWidget extends StatefulWidget {
+
+  final String text;
+
+  DemoStateWidget(this.text);
+
+  @override
+  _DemoStateWidgetState createState() => _DemoStateWidgetState(text);
+}
+
+class _DemoStateWidgetState extends State<DemoStateWidget> {
+
+  String text;
+
+  _DemoStateWidgetState(this.text);
+
+  @override
+  void initState() {
+    ///初始化，这个函数在生命周期中只调用一次
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    ///销毁
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    ///在initState之后调 Called when a dependency of this [State] object changes.
+    super.didChangeDependencies();
+    new Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        text = "这就变了数值";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(text ?? "这就是有状态DMEO"),
+    );
+  }
+}
