e6fa3f37a2520e743f1178f58fbfdf12f1f0d0d7
guoshuyu <359369982@qq.com>
2018-08-06 11:28:21 +0800

    增加注释

--- lib/widget/GSYPullLoadWidget.dart
+++ lib/widget/GSYPullLoadWidget.dart
@@ -41,7 +41,9 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
 
   @override
   void initState() {
+    ///增加滑动监听
     _scrollController.addListener(() {
+      ///判断当前滑动位置是不是到达底部，触发加载更多回调
       if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
         if (this.onLoadMore != null && this.control.needLoadMore) {
           this.onLoadMore();
@@ -51,41 +53,66 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
     super.initState();
   }
 
+  ///根据配置状态返回实际列表数量
+  ///实际上这里可以根据你的需要做更多的处理
+  ///比如多个头部，是否需要空页面，是否需要显示加载更多。
   _getListCount() {
+    ///是否需要头部
     if (control.needHeader) {
+      ///如果需要头部，用Item 0 的 Widget 作为ListView的头部
+      ///列表数量大于0时，因为头部和底部加载更多选项，需要对列表数据总数+2
       return (control.dataList.length > 0) ? control.dataList.length + 2 : control.dataList.length + 1;
     } else {
-      if(control.dataList.length == 0) {
+      ///如果不需要头部，在没有数据时，固定返回数量1用于空页面呈现
+      if (control.dataList.length == 0) {
         return 1;
       }
+
+      ///如果有数据,因为部加载更多选项，需要对列表数据总数+1
       return (control.dataList.length > 0) ? control.dataList.length + 1 : control.dataList.length;
+    }
+  }
+
+  ///根据配置状态返回实际列表渲染Item
+  _getItem(int index) {
+    if (!control.needHeader && index == control.dataList.length && control.dataList.length != 0) {
+      ///如果不需要头部，并且数据不为0，当index等于数据长度时，渲染加载更多Item（因为index是从0开始）
+      return _buildProgressIndicator();
+    } else if (control.needHeader && index == _getListCount() - 1 && control.dataList.length != 0) {
+      ///如果需要头部，并且数据不为0，当index等于实际渲染长度 - 1时，渲染加载更多Item（因为index是从0开始）
+      return _buildProgressIndicator();
+    } else if (!control.needHeader && control.dataList.length == 0) {
+      ///如果不需要头部，并且数据为0，渲染空页面
+      return _buildEmpty();
+    } else {
+      ///回调外部正常渲染Item，如果这里有需要，可以直接返回相对位置的index
+      return itemBuilder(context, index);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return new RefreshIndicator(
+      ///GlobalKey，用户外部获取RefreshIndicator的State，做显示刷新
       key: refreshKey,
+      ///下拉刷新触发，返回的是一个Future
       onRefresh: onRefresh,
       child: new ListView.builder(
+        ///保持ListView任何情况都能滚动，解决在RefreshIndicator的兼容问题。
         physics: const AlwaysScrollableScrollPhysics(),
+        ///根据状态返回子孔健
         itemBuilder: (context, index) {
-          if (!control.needHeader && index == control.dataList.length && control.dataList.length != 0) {
-            return _buildProgressIndicator();
-          } else if (control.needHeader && index == _getListCount() - 1 && control.dataList.length != 0) {
-            return _buildProgressIndicator();
-          } else if (!control.needHeader && control.dataList.length == 0) {
-            return _buildEmpty();
-          } else {
-            return itemBuilder(context, index);
-          }
+          return _getItem(index);
         },
+        ///根据状态返回数量
         itemCount: _getListCount(),
+        ///滑动监听
         controller: _scrollController,
       ),
     );
   }
 
+  ///空页面
   Widget _buildEmpty() {
     return new Container(
       height: MediaQuery.of(context).size.height - 100,
@@ -104,19 +131,28 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
     );
   }
 
+  ///上拉加载更多
   Widget _buildProgressIndicator() {
+    ///是否需要显示上拉加载更多的loading
     Widget bottomWidget = (control.needLoadMore)
         ? new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            new SpinKitRotatingCircle(color: Color(GSYColors.primaryValue)),
+            ///loading框
+            new SpinKitRotatingCircle(color: Color(0xFF24292E)),
             new Container(
               width: 5.0,
             ),
+            ///加载中文本
             new Text(
-              GSYStrings.load_more_text,
-              style: GSYConstant.smallTextBold,
+              "加载中···",
+              style: TextStyle(
+                color: Color(0xFF121917),
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
             )
           ])
-        : new Text(GSYStrings.load_more_not, style: GSYConstant.smallTextBold);
+          /// 不需要加载
+        : new Container();
     return new Padding(
       padding: const EdgeInsets.all(20.0),
       child: new Center(
