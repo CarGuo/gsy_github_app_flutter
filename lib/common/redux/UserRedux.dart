5b46cb0dfa22ef7fc5570e528756889fe4ccda60
guoshuyu <359369982@qq.com>
2018-08-06 16:04:30 +0800

    增加注释

--- lib/common/redux/UserRedux.dart
+++ lib/common/redux/UserRedux.dart
@@ -7,16 +7,21 @@ import 'package:redux/redux.dart';
  * Created by guoshuyu
  * Date: 2018-07-16
  */
-
+/// redux 的 combineReducers, 通过 TypedReducer 将 UpdateUserAction 与 reducers 关联起来
 final UserReducer = combineReducers<User>([
   TypedReducer<User, UpdateUserAction>(_updateLoaded),
 ]);
 
+/// 如果有 UpdateUserAction 发起一个请求时
+/// 就会调用到 _updateLoaded
+/// _updateLoaded 这里接受一个新的userInfo，并返回
 User _updateLoaded(User user, action) {
   user = action.userInfo;
   return user;
 }
 
+///定一个 UpdateUserAction ，用于发起 userInfo 的的改变
+///类名随你喜欢定义，只要通过上面TypedReducer绑定就好
 class UpdateUserAction {
   final User userInfo;
   UpdateUserAction(this.userInfo);
