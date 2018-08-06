86c084d04b9854c953bc5592c951a6069e09b47d
guoshuyu <359369982@qq.com>
2018-08-06 15:03:20 +0800

    增加注释

--- lib/common/model/Template.g.dart
+++ lib/common/model/Template.g.dart
@@ -6,8 +6,13 @@ part of 'Template.dart';
 // Generator: JsonSerializableGenerator
 // **************************************************************************
 
-Template _$TemplateFromJson(Map<String, dynamic> json) => new Template();
+Template _$TemplateFromJson(Map<String, dynamic> json) => new Template(
+    json['name'] as String, json['id'] as int, json['push_id'] as int);
 
 abstract class _$TemplateSerializerMixin {
-  Map<String, dynamic> toJson() => <String, dynamic>{};
+  String get name;
+  int get id;
+  int get pushId;
+  Map<String, dynamic> toJson() =>
+      <String, dynamic>{'name': name, 'id': id, 'push_id': pushId};
 }
