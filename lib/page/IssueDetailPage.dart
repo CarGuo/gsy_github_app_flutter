ea44f5bc990cf1ca5db3ca05c7c9ef8a8461df0b
Shuyu Guo <359369982@qq.com>
2018-08-09 20:20:53 +0800

    fix #18

--- lib/page/IssueDetailPage.dart
+++ lib/page/IssueDetailPage.dart
@@ -226,21 +226,30 @@ class _IssueDetailPageState extends GSYListState<IssueDetailPage> {
   _replyIssue() {
     //回复 Info
     String content = "";
-    CommonUtils.showEditDialog(context, GSYStrings.issue_reply_issue, null, (replyContent) {
-      content = replyContent;
-    }, () {
-      if (content == null || content.trim().length == 0) {
-        Fluttertoast.showToast(msg: GSYStrings.issue_edit_issue_content_not_be_null);
-        return;
-      }
-      CommonUtils.showLoadingDialog(context);
-      //提交评论
-      IssueDao.addIssueCommentDao(userName, reposName, issueNum, content).then((result) {
-        showRefreshLoading();
-        Navigator.pop(context);
-        Navigator.pop(context);
-      });
-    }, needTitle: false);
+    CommonUtils.showEditDialog(
+      context,
+      GSYStrings.issue_reply_issue,
+      null,
+      (replyContent) {
+        content = replyContent;
+      },
+      () {
+        if (content == null || content.trim().length == 0) {
+          Fluttertoast.showToast(msg: GSYStrings.issue_edit_issue_content_not_be_null);
+          return;
+        }
+        CommonUtils.showLoadingDialog(context);
+        //提交评论
+        IssueDao.addIssueCommentDao(userName, reposName, issueNum, content).then((result) {
+          showRefreshLoading();
+          Navigator.pop(context);
+          Navigator.pop(context);
+        });
+      },
+      needTitle: false,
+      titleController: issueInfoTitleControl,
+      valueController: issueInfoValueControl,
+    );
   }
 
   _getBottomWidget() {
