13e5ab0b85158ad3d5027dfc41e409806eeb0a15
Shuyu Guo <359369982@qq.com>
2018-08-12 00:22:37 +0800

    启动图

--- lib/common/ab/SqlManager.dart
+++ lib/common/ab/SqlManager.dart
@@ -10,9 +10,9 @@ import 'package:sqflite/sqflite.dart';
  */
 
 class SqlManager {
-  static final _VERSION = 1;
+  static const _VERSION = 1;
 
-  static final _NAME = "gsy_github_app_flutter.db";
+  static const _NAME = "gsy_github_app_flutter.db";
 
   static Database _database;
 
