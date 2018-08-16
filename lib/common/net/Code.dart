cb9e02ad4c023cd8edfe96faddb8393ba4057669
guoshuyu <359369982@qq.com>
2018-08-16 17:00:05 +0800

    add event bus.

--- lib/common/net/Code.dart
+++ lib/common/net/Code.dart
@@ -1,6 +1,5 @@
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:gsy_github_app_flutter/common/style/GSYStyle.dart';
-
+import 'package:event_bus/event_bus.dart';
+import 'package:gsy_github_app_flutter/common/event/HttpErrorEvent.dart';
 ///错误编码
 class Code {
   ///网络错误
@@ -14,43 +13,13 @@ class Code {
 
   static const SUCCESS = 200;
 
+  static final EventBus eventBus = new EventBus();
+
   static errorHandleFunction(code, message, noTip) {
-    switch (code) {
-      case NETWORK_ERROR:
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error);
-        }
-        return GSYStrings.network_error;
-      case 401:
-        //TODO 授权逻辑
-        /*if (Actions.currentScene !== 'LoginPage') {
-          Actions.reset("LoginPage");
-        }*/
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error_401);
-        }
-        return GSYStrings.network_error_401; //401 Unauthorized
-      case 403:
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error_403);
-        }
-        return GSYStrings.network_error_403;
-      case 404:
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error_404);
-        }
-        return GSYStrings.network_error_404;
-      case NETWORK_TIMEOUT:
-        //超时
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error_timeout);
-        }
-        return GSYStrings.network_error_timeout;
-      default:
-        if(!noTip) {
-          Fluttertoast.showToast(msg: GSYStrings.network_error_unknown + " " + message);
-        }
-        return GSYStrings.network_error_unknown;
+    if(noTip) {
+      return message;
     }
+    eventBus.fire(new HttpErrorEvent(code, message));
+    return message;
   }
 }
--- lib/common/style/GSYStyle.dart
+++ lib/common/style/GSYStyle.dart
@@ -207,18 +207,6 @@ class GSYConstant {
   );
 }
 
-///字符文本
-class GSYStrings {
-
-
-  static const String network_error_401 = "[401错误可能: 未授权 \\ 授权登录失败 \\ 登录过期]";
-  static const String network_error_403 = "403权限错误";
-  static const String network_error_404 = "404错误";
-  static const String network_error_timeout = "请求超时";
-  static const String network_error_unknown = "其他异常";
-  static const String network_error = "网络错误";
-}
-
 class GSYICons {
   static const String FONT_FAMILY = 'wxcIconFont';
 
