c2f9f4f90a794c2ed7a02609f02ea793b0689fca
Shuyu Guo <359369982@qq.com>
2018-08-19 16:21:25 +0800

    注释

--- lib/common/redux/GSYState.dart
+++ lib/common/redux/GSYState.dart
@@ -25,17 +25,22 @@ class GSYState {
   ///用户接受到的事件列表
   List<TrendingRepoModel> trendList = new List();
 
+  ///主题数据
   ThemeData themeData;
 
+  ///语言
   Locale locale;
 
+  ///当前手机平台默认语言
   Locale platformLocale;
 
   ///构造方法
   GSYState({this.userInfo, this.eventList, this.trendList, this.themeData, this.locale});
 }
 
-///通过 Reducer 创建 store 保存的 GSYState
+///创建 Reducer
+///源码中 Reducer 是一个方法 typedef State Reducer<State>(State state, dynamic action);
+///我们自定义了 appReducer 用于创建 store
 GSYState appReducer(GSYState state, action) {
   return GSYState(
     ///通过 UserReducer 将 GSYState 内的 userInfo 和 action 关联在一起
@@ -47,8 +52,10 @@ GSYState appReducer(GSYState state, action) {
     ///通过 TrendReducer 将 GSYState 内的 trendList 和 action 关联在一起
     trendList: TrendReducer(state.trendList, action),
 
+    ///通过 ThemeDataReducer 将 GSYState 内的 themeData 和 action 关联在一起
     themeData: ThemeDataReducer(state.themeData, action),
 
+    ///通过 LocaleReducer 将 GSYState 内的 locale 和 action 关联在一起
     locale: LocaleReducer(state.locale, action),
   );
 }
