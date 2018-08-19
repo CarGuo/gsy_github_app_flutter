608e63e23520c6e9ea5070df3f58e5b862449608
Shuyu Guo <359369982@qq.com>
2018-08-19 16:56:21 +0800

    注释

--- lib/common/localization/GSYLocalizationsDelegate.dart
+++ lib/common/localization/GSYLocalizationsDelegate.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:gsy_github_app_flutter/common/localization/DefaultLocalizations.dart';
 
 /**
+ * 多语言代理
  * Created by guoshuyu
  * Date: 2018-08-15
  */
@@ -14,9 +15,11 @@ class GSYLocalizationsDelegate extends LocalizationsDelegate<GSYLocalizations> {
 
   @override
   bool isSupported(Locale locale) {
+    ///支持中文和英语
     return ['en', 'zh'].contains(locale.languageCode);
   }
 
+  ///根据locale，创建一个对象用于提供当前locale下的文本显示
   @override
   Future<GSYLocalizations> load(Locale locale) {
     return new SynchronousFuture<GSYLocalizations>(new GSYLocalizations(locale));
@@ -27,5 +30,6 @@ class GSYLocalizationsDelegate extends LocalizationsDelegate<GSYLocalizations> {
     return false;
   }
 
+  ///全局静态的代理
   static GSYLocalizationsDelegate delegate = new GSYLocalizationsDelegate();
 }
