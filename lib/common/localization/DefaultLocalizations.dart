619ae51fda914f5cb055757999b04d25f6f2afe2
Shuyu Guo <359369982@qq.com>
2018-08-19 18:09:38 +0800

    注释

--- lib/common/localization/DefaultLocalizations.dart
+++ lib/common/localization/DefaultLocalizations.dart
@@ -5,11 +5,14 @@ import 'package:gsy_github_app_flutter/common/style/GSYStringBase.dart';
 import 'package:gsy_github_app_flutter/common/style/GSYStringEn.dart';
 import 'package:gsy_github_app_flutter/common/style/GSYStringZh.dart';
 
+///自定义多语言实现
 class GSYLocalizations {
   final Locale locale;
 
   GSYLocalizations(this.locale);
 
+  ///根据不同 locale.languageCode 加载不同语言对应
+  ///GSYStringEn和GSYStringZh都继承了GSYStringBase
   static Map<String, GSYStringBase> _localizedValues = {
     'en': new GSYStringEn(),
     'zh': new GSYStringZh(),
@@ -19,6 +22,8 @@ class GSYLocalizations {
     return _localizedValues[locale.languageCode];
   }
 
+  ///通过 Localizations 加载当前的 GSYLocalizations
+  ///获取对应的 GSYStringBase
   static GSYLocalizations of(BuildContext context) {
     return Localizations.of(context, GSYLocalizations);
   }
