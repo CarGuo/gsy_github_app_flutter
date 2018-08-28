a57755ff90c239f32186f4b934079aecd9d1ca30
yinrenjie <16231189@qq.com>
2018-08-28 09:50:26 +0800

    代码高亮

--- lib/widget/GSYMarkdownWidget.dart
+++ lib/widget/GSYMarkdownWidget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:gsy_github_app_flutter/common/style/GSYStyle.dart';
 import 'package:gsy_github_app_flutter/common/utils/CommonUtils.dart';
+import 'package:gsy_github_app_flutter/widget/syntax_highlighter.dart';
 
 /**
  * 代码详情
@@ -23,18 +24,21 @@ class GSYMarkdownWidget extends StatelessWidget {
   GSYMarkdownWidget({this.markdownData = "", this.style = DARK_WHITE});
 
   _getCommonSheet(BuildContext context, Color codeBackground) {
-    MarkdownStyleSheet markdownStyleSheet = MarkdownStyleSheet.fromTheme(Theme.of(context));
+    MarkdownStyleSheet markdownStyleSheet =
+        MarkdownStyleSheet.fromTheme(Theme.of(context));
     return markdownStyleSheet
         .copyWith(
             codeblockDecoration: new BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(4.0)),
                 color: codeBackground,
-                border: new Border.all(color: Color(GSYColors.subTextColor), width: 0.3)))
+                border: new Border.all(
+                    color: Color(GSYColors.subTextColor), width: 0.3)))
         .copyWith(
             blockquoteDecoration: new BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(4.0)),
                 color: Color(GSYColors.subTextColor),
-                border: new Border.all(color: Color(GSYColors.subTextColor), width: 0.3)),
+                border: new Border.all(
+                    color: Color(GSYColors.subTextColor), width: 0.3)),
             blockquote: GSYConstant.smallTextWhite);
   }
 
@@ -123,7 +127,9 @@ class GSYMarkdownWidget extends StatelessWidget {
           String match = imageMatch.replaceAll("\)", "?raw=true)");
           if (!match.contains(".svg") && match.contains("http")) {
             ///增加点击
-            String src = match.replaceAll(new RegExp(r'!\[.*\]\('), "").replaceAll(")", "");
+            String src = match
+                .replaceAll(new RegExp(r'!\[.*\]\('), "")
+                .replaceAll(")", "");
             String actionMatch = "[$match]($src)";
             match = actionMatch;
           } else {
@@ -145,7 +151,9 @@ class GSYMarkdownWidget extends StatelessWidget {
           for (Match srcMatch in srcTags) {
             String srcString = srcMatch.group(0);
             if (srcString != null && srcString.contains("http")) {
-              String newSrc = srcString.substring(srcString.indexOf("http"), srcString.length - 1) + "?raw=true";
+              String newSrc = srcString.substring(
+                      srcString.indexOf("http"), srcString.length - 1) +
+                  "?raw=true";
 
               ///增加点击
               match = "[![]($newSrc)]($newSrc)";
@@ -181,9 +189,7 @@ class GSYMarkdownWidget extends StatelessWidget {
 class GSYHighlighter extends SyntaxHighlighter {
   @override
   TextSpan format(String source) {
-    return new TextSpan(
-      text: source,
-      style: GSYConstant.smallTextWhite,
-    );
+    print(source);
+    return new DartSyntaxHighlighter().format(source);
   }
 }
