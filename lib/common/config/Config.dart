38aaba27e59341fe5f6f290035129b0b509fa977
guoshuyu <359369982@qq.com>
2018-10-17 17:16:32 +0800

    flag for android view

--- lib/common/config/Config.dart
+++ lib/common/config/Config.dart
@@ -1,6 +1,7 @@
 class Config {
   static const PAGE_SIZE = 20;
   static const DEBUG = true;
+  static const USE_NATIVE_WEBVIEW = false;
 
 
   /// //////////////////////////////////////常量////////////////////////////////////// ///
