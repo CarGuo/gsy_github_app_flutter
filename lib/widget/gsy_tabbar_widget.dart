82d1cb8e85a163771d2399bf78812f8cb0ef1743
guoshuyu <359369982@qq.com>
2019-04-24 18:25:57 +0800

    处理ios的底部safeArea

--- lib/widget/gsy_tabbar_widget.dart
+++ lib/widget/gsy_tabbar_widget.dart
@@ -56,7 +56,8 @@ class GSYTabBarWidget extends StatefulWidget {
       );
 }
 
-class _GSYTabBarState extends State<GSYTabBarWidget> with SingleTickerProviderStateMixin {
+class _GSYTabBarState extends State<GSYTabBarWidget>
+    with SingleTickerProviderStateMixin {
   final int _type;
 
   final List<Widget> _tabViews;
@@ -91,7 +92,8 @@ class _GSYTabBarState extends State<GSYTabBarWidget> with SingleTickerProviderSt
   @override
   void initState() {
     super.initState();
-    _tabController = new TabController(vsync: this, length: widget.tabItems.length);
+    _tabController =
+        new TabController(vsync: this, length: widget.tabItems.length);
   }
 
   ///整个页面dispose时，记得把控制器也dispose掉，释放内存
@@ -107,7 +109,8 @@ class _GSYTabBarState extends State<GSYTabBarWidget> with SingleTickerProviderSt
       ///顶部tab bar
       return new Scaffold(
         floatingActionButton: _floatingActionButton,
-        persistentFooterButtons: _tarWidgetControl == null ? [] : _tarWidgetControl.footerButton,
+        persistentFooterButtons:
+            _tarWidgetControl == null ? [] : _tarWidgetControl.footerButton,
         appBar: new AppBar(
           backgroundColor: Theme.of(context).primaryColor,
           title: _title,
@@ -117,7 +120,8 @@ class _GSYTabBarState extends State<GSYTabBarWidget> with SingleTickerProviderSt
               indicatorColor: _indicatorColor,
               onTap: (index) {
                 _onPageChanged?.call(index);
-                _pageController.jumpTo(MediaQuery.of(context).size.width * index);
+                _pageController
+                    .jumpTo(MediaQuery.of(context).size.width * index);
               }),
         ),
         body: new PageView(
@@ -149,15 +153,18 @@ class _GSYTabBarState extends State<GSYTabBarWidget> with SingleTickerProviderSt
         bottomNavigationBar: new Material(
           //为了适配主题风格，包一层Material实现风格套用
           color: Theme.of(context).primaryColor, //底部导航栏主题颜色
-          child: new TabBar(
-            //TabBar导航标签，底部导航放到Scaffold的bottomNavigationBar中
-            controller: _tabController, //配置控制器
-            tabs: widget.tabItems,
-            indicatorColor: _indicatorColor,
-            onTap: (index) {
-              _onPageChanged?.call(index);
-              _pageController.jumpTo(MediaQuery.of(context).size.width * index);
-            }, //tab标签的下划线颜色
+          child: new SafeArea(
+            child: new TabBar(
+              //TabBar导航标签，底部导航放到Scaffold的bottomNavigationBar中
+              controller: _tabController, //配置控制器
+              tabs: widget.tabItems,
+              indicatorColor: _indicatorColor,
+              onTap: (index) {
+                _onPageChanged?.call(index);
+                _pageController
+                    .jumpTo(MediaQuery.of(context).size.width * index);
+              }, //tab标签的下划线颜色
+            ),
           ),
         ));
   }
