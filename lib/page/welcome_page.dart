acae1899a5e648b5b7b763ee3b042d4cb509265c
guoshuyu <359369982@qq.com>
2019-04-26 11:43:51 +0800

    add lottie

--- lib/page/welcome_page.dart
+++ lib/page/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_lottie/flutter_lottie.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:gsy_github_app_flutter/common/dao/user_dao.dart';
 import 'package:gsy_github_app_flutter/common/redux/gsy_state.dart';
@@ -22,21 +23,21 @@ class WelcomePage extends StatefulWidget {
   _WelcomePageState createState() => _WelcomePageState();
 }
 
-class _WelcomePageState extends State<WelcomePage>  {
-
+class _WelcomePageState extends State<WelcomePage> {
   bool hadInit = false;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    if(hadInit) {
+    if (hadInit) {
       return;
     }
     hadInit = true;
+
     ///防止多次进入
     Store<GSYState> store = StoreProvider.of(context);
     CommonUtils.initStatusBarHeight(context);
-    new Future.delayed(const Duration(seconds: 2), () {
+    new Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
       UserDao.initUserInfo(store).then((res) {
         if (res != null && res.result) {
           NavigatorUtils.goHome(context);
@@ -48,18 +49,42 @@ class _WelcomePageState extends State<WelcomePage>  {
     });
   }
 
+  void onViewCreatedFile(LottieController controller) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return StoreBuilder<GSYState>(
       builder: (context, store) {
+        double size = 200;
         return new Container(
           color: Color(GSYColors.white),
-          child: new Center(
-            child: new Image(image: new AssetImage('static/images/welcome.png')),
+          child: Stack(
+            children: <Widget>[
+              new Center(
+                child: new Image(
+                    image: new AssetImage('static/images/welcome.png')),
+              ),
+              new Align(
+                alignment: Alignment.bottomCenter,
+                child: new Container(
+                  width: size,
+                  height: size,
+                  color: Colors.white,
+                  child: LottieView.fromFile(
+                    filePath: "static/file/rejection2.json",
+                    autoPlay: true,
+                    loop: true,
+                    reverse: false,
+                    onViewCreated: onViewCreatedFile,
+                  ),
+                ),
+              )
+            ],
           ),
         );
       },
     );
   }
-
 }
