090f56acff6830cba152760e17845640694d5bdd
guoshuyu <359369982@qq.com>
2019-05-27 17:08:27 +0800

    增加ios默认下拉刷新支持

--- lib/widget/gsy_pull_new_load_widget.dart
+++ lib/widget/gsy_pull_new_load_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart' as IOS;
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:gsy_github_app_flutter/common/style/gsy_style.dart';
@@ -17,17 +18,23 @@ class GSYPullLoadWidget extends StatefulWidget {
   ///控制器，比如数据和一些配置
   final GSYPullLoadWidgetControl control;
 
+  final ScrollController scrollController;
+
+  final bool userIos;
+
   ///刷新key
   final Key refreshKey;
 
-  GSYPullLoadWidget(this.control, this.itemBuilder, this.onRefresh, this.onLoadMore, {this.refreshKey});
+  GSYPullLoadWidget(
+      this.control, this.itemBuilder, this.onRefresh, this.onLoadMore,
+      {this.refreshKey, this.scrollController, this.userIos = false});
 
   @override
   _GSYPullLoadWidgetState createState() => _GSYPullLoadWidgetState();
 }
 
 class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
-  final ScrollController _scrollController = new ScrollController();
+  ScrollController _scrollController;
 
   bool isRefreshing = false;
 
@@ -35,15 +42,19 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
 
   @override
   void initState() {
+    _scrollController = widget.scrollController ?? new ScrollController();
+
     ///增加滑动监听
     _scrollController.addListener(() {
       ///判断当前滑动位置是不是到达底部，触发加载更多回调
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         if (widget.control.needLoadMore) {
           handleLoadMore();
         }
       }
     });
+
     widget.control.addListener(() {
       setState(() {});
       try {
@@ -66,7 +77,9 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
     if (widget.control.needHeader) {
       ///如果需要头部，用Item 0 的 Widget 作为ListView的头部
       ///列表数量大于0时，因为头部和底部加载更多选项，需要对列表数据总数+2
-      return (widget.control.dataList.length > 0) ? widget.control.dataList.length + 2 : widget.control.dataList.length + 1;
+      return (widget.control.dataList.length > 0)
+          ? widget.control.dataList.length + 2
+          : widget.control.dataList.length + 1;
     } else {
       ///如果不需要头部，在没有数据时，固定返回数量1用于空页面呈现
       if (widget.control.dataList.length == 0) {
@@ -74,19 +87,26 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
       }
 
       ///如果有数据,因为部加载更多选项，需要对列表数据总数+1
-      return (widget.control.dataList.length > 0) ? widget.control.dataList.length + 1 : widget.control.dataList.length;
+      return (widget.control.dataList.length > 0)
+          ? widget.control.dataList.length + 1
+          : widget.control.dataList.length;
     }
   }
 
   ///根据配置状态返回实际列表渲染Item
   _getItem(int index) {
-    if (!widget.control.needHeader && index == widget.control.dataList.length && widget.control.dataList.length != 0) {
+    if (!widget.control.needHeader &&
+        index == widget.control.dataList.length &&
+        widget.control.dataList.length != 0) {
       ///如果不需要头部，并且数据不为0，当index等于数据长度时，渲染加载更多Item（因为index是从0开始）
       return _buildProgressIndicator();
-    } else if (widget.control.needHeader && index == _getListCount() - 1 && widget.control.dataList.length != 0) {
+    } else if (widget.control.needHeader &&
+        index == _getListCount() - 1 &&
+        widget.control.dataList.length != 0) {
       ///如果需要头部，并且数据不为0，当index等于实际渲染长度 - 1时，渲染加载更多Item（因为index是从0开始）
       return _buildProgressIndicator();
-    } else if (!widget.control.needHeader && widget.control.dataList.length == 0) {
+    } else if (!widget.control.needHeader &&
+        widget.control.dataList.length == 0) {
       ///如果不需要头部，并且数据为0，渲染空页面
       return _buildEmpty();
     } else {
@@ -148,6 +168,33 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.userIos) {
+      ///用ios模式的下拉刷新
+      return CustomScrollView(
+        controller: _scrollController,
+        ///回弹效果
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        slivers: <Widget>[
+          ///控制显示刷新的 CupertinoSliverRefreshControl
+          IOS.CupertinoSliverRefreshControl(
+            onRefresh: handleRefresh,
+          ),
+          SliverSafeArea(
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return _getItem(index);
+                },
+                childCount: _getListCount(),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
     return new RefreshIndicator(
       ///GlobalKey，用户外部获取RefreshIndicator的State，做显示刷新
       key: widget.refreshKey,
@@ -181,10 +228,14 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
         children: <Widget>[
           FlatButton(
             onPressed: () {},
-            child: new Image(image: new AssetImage(GSYICons.DEFAULT_USER_ICON), width: 70.0, height: 70.0),
+            child: new Image(
+                image: new AssetImage(GSYICons.DEFAULT_USER_ICON),
+                width: 70.0,
+                height: 70.0),
           ),
           Container(
-            child: Text(CommonUtils.getLocale(context).app_empty, style: GSYConstant.normalText),
+            child: Text(CommonUtils.getLocale(context).app_empty,
+                style: GSYConstant.normalText),
           ),
         ],
       ),
@@ -195,23 +246,26 @@ class _GSYPullLoadWidgetState extends State<GSYPullLoadWidget> {
   Widget _buildProgressIndicator() {
     ///是否需要显示上拉加载更多的loading
     Widget bottomWidget = (widget.control.needLoadMore)
-        ? new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            ///loading框
-            new SpinKitRotatingCircle(color: Theme.of(context).primaryColor),
-            new Container(
-              width: 5.0,
-            ),
+        ? new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+                ///loading框
+                new SpinKitRotatingCircle(
+                    color: Theme.of(context).primaryColor),
+                new Container(
+                  width: 5.0,
+                ),
 
-            ///加载中文本
-            new Text(
-              CommonUtils.getLocale(context).load_more_text,
-              style: TextStyle(
-                color: Color(0xFF121917),
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ])
+                ///加载中文本
+                new Text(
+                  CommonUtils.getLocale(context).load_more_text,
+                  style: TextStyle(
+                    color: Color(0xFF121917),
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ])
 
         /// 不需要加载
         : new Container();
