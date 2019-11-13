235ac15f518de93c162d58c3f9a36925551633a3
guoshuyu <359369982@qq.com>
2019-11-13 15:44:34 +0800

    添加测试

--- lib/test/demo_tab_page.dart
+++ lib/test/demo_tab_page.dart
@@ -37,15 +37,19 @@ class _DemoTabPageState extends State<DemoTabPage> {
     return [
       Container(
         color: Colors.blue,
+        child: KeepAliveList(),
       ),
       Container(
         color: Colors.red,
+        child: KeepAliveList(),
       ),
       Container(
         color: Colors.amber,
+        child: KeepAliveList(),
       ),
     ];
   }
+
 }
 
 enum TabType { bottom, top }
@@ -189,3 +193,40 @@ class _GSYTabBarState extends State<TabWidget>
         bottomNavigationBar: _getBottomNavByType());
   }
 }
+
+class KeepAliveList extends StatefulWidget {
+  @override
+  _KeepAliveListState createState() => _KeepAliveListState();
+}
+
+class _KeepAliveListState extends State<KeepAliveList> with AutomaticKeepAliveClientMixin {
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        return new Card(
+          ///设置阴影的深度
+          elevation: 5.0,
+          ///增加圆角
+          shape: new RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          color: Colors.white,
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+          child: new Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            height: 80,
+            child: new Text("显示文本 ${index}"),
+          ),
+        );
+      },
+      itemCount: 20,
+    );
+  }
+}
+
