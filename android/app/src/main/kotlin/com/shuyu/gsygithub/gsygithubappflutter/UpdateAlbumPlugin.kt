4c70dca0b896b42d9ec77f9c0750812ec684e617
guoshuyu <359369982@qq.com>
2019-12-25 18:15:43 +0800

    适配  1.12.13+hotfix.6

--- android/app/src/main/kotlin/com/shuyu/gsygithub/gsygithubappflutter/UpdateAlbumPlugin.kt
+++ android/app/src/main/kotlin/com/shuyu/gsygithub/gsygithubappflutter/UpdateAlbumPlugin.kt
@@ -2,35 +2,56 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
-import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
-object UpdateAlbumPlugin {
+class UpdateAlbumPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
 
     /** Channel名称  **/
-    private const val ChannelName = "com.shuyu.gsygithub.gsygithubflutter/UpdateAlbumPlugin"
 
-    /**
-     * 注册Toast插件
-     * @param context 上下文对象
-     * @param messenger 数据信息交流对象
-     */
-    @JvmStatic
-    fun register(context: Context, messenger: BinaryMessenger) = MethodChannel(messenger, ChannelName).setMethodCallHandler { methodCall, result ->
+    private var channel: MethodChannel? = null
+    private var context: Context? = null
+
+    companion object {
+
+        private val sChannelName = "com.shuyu.gsygithub.gsygithubflutter/UpdateAlbumPlugin"
+
+        fun registerWith(registrar: Registrar) {
+            val instance = UpdateAlbumPlugin()
+            instance.channel = MethodChannel(registrar.messenger(), sChannelName)
+            instance.context = registrar.context()
+            instance.channel?.setMethodCallHandler(instance)
+        }
+    }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(
+                binding.flutterEngine.dartExecutor, sChannelName)
+        context = binding.applicationContext
+        channel!!.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        channel = null
+    }
+
+    override fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
         when (methodCall.method) {
             "updateAlbum" -> {
                 val path: String? = methodCall.argument("path")
                 val name: String? = methodCall.argument("name")
                 try {
-                    MediaStore.Images.Media.insertImage(context.contentResolver, path, name, null)
+                    MediaStore.Images.Media.insertImage(context?.contentResolver, path, name, null)
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
                 // 最后通知图库更新
-                context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://$path")))
+                context?.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://$path")))
             }
         }
-        result.success(null) //没有返回值，所以直接返回为null
+        result.success(null)
     }
-
 }
