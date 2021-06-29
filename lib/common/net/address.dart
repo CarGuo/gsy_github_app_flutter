1b147061b6439abdf0732f61ad39568acd358cc1
guoshuyu <359369982@qq.com>
2021-06-29 16:37:17 +0800

    update update Url

--- lib/common/net/address.dart
+++ lib/common/net/address.dart
@@ -6,7 +6,7 @@ class Address {
   static const String host = "https://api.github.com/";
   static const String hostWeb = "https://github.com/";
   static const String graphicHost = 'https://ghchart.rshah.org/';
-  static const String updateUrl = 'https://www.pgyer.com/guqa';
+  static const String updateUrl = 'https://gitee.com/CarGuo/GSYGithubAppFlutter/releases';
 
   ///获取授权  post
   static getAuthorization() {
