2e0503daa1daea5a7cfed970cf7a722bf7488392
guoshuyu <359369982@qq.com>
2021-07-06 09:16:04 +0800

    add resamplingEnabled

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:gsy_github_app_flutter/app.dart';
 import 'package:gsy_github_app_flutter/env/config_wrapper.dart';
@@ -10,6 +11,8 @@ import 'package:gsy_github_app_flutter/page/error_page.dart';
 import 'env/prod.dart';
 
 void main() {
+  ///屏幕刷新率和显示率不一致时的优化
+  GestureBinding.instance?.resamplingEnabled = true;
   runZonedGuarded(() {
     ErrorWidget.builder = (FlutterErrorDetails details) {
       Zone.current.handleUncaughtError(details.exception, details.stack!);
