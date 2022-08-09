7a8d02bb9af5b71df41e1b60d5d7acc15040f564
guoshuyu <359369982@qq.com>
2022-08-09 09:18:00 +0800

    update depend device info

--- lib/common/utils/common_utils.dart
+++ lib/common/utils/common_utils.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
--- lib/page/debug/debug_label.dart
+++ lib/page/debug/debug_label.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:gsy_github_app_flutter/common/localization/default_localizations.dart';
 import 'package:gsy_github_app_flutter/common/utils/common_utils.dart';
